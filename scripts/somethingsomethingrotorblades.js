d3cdd48c69b21afecdf0a6757c36b3efddada23f
Roomba <61640498+Meltdown-Altair@users.noreply.github.com>
2021-03-11 21:19:04 +0700

    Update somethingsomethingrotorblades.js

--- scripts/somethingsomethingrotorblades.js
+++ scripts/somethingsomethingrotorblades.js
@@ -1,36 +1,72 @@
-//this is NOT the complete definition for this block! see content/blocks/scatter-silo.hjson for the stats and other properties.
-
-//create a simple shockwave effect
-const siloLaunchEffect = newEffect(20, e => {
-    Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
-    Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
-    Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
+//Bstuka
+const t3A_copter = extend(UnitType, "t3A_copter", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"heavy-armaments-t3A_copter-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+		Draw.rect(
+			"heavy-armaments-t3A_copter-rotor-outline",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+	}
 });
 
-//create the block type
-const silo = extendContent(Block, "scatter-silo", {
-    //override the method to build configuration
-    buildConfiguration(tile, table){
-        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            //configure the tile to signal that it has been pressed (this sync on client to server)
-            tile.configure(0)
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()))
-    },
+//Orgion
+const t4A_copter = extend(UnitType, "t4A_copter", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"heavy-armaments-t4A_copter-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 2),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 2),
+			Time.time * -15
+		);
+		Draw.rect(
+			"heavy-armaments-t4A_copter-rotor-outline",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 2),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 2),
+			Time.time * -15);
+	}
+});
 
-    //override configure event
-    configured(tile, value){
-        //make sure this silo has the items it needs to fire
-        if(tile.entity.cons.valid()){
-            //make this effect occur at the tile location
-            Effects.effect(siloLaunchEffect, tile)
+//Bathesdea
+const t5A_copter = extend(UnitType, "t5A_copter", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"heavy-armaments-t5A_copter-rotor-1-1",
+			unit.x + Angles.trnsx(unit.rotation - 90, 4, 5),
+			unit.y + Angles.trnsy(unit.rotation - 90, 4, 5),
+			Time.time * 18
+		);
+		Draw.rect(
+			"heavy-armaments-t5A_copter-rotor-1-2",
+			unit.x + Angles.trnsx(unit.rotation - 90, 4, 5),
+			unit.y + Angles.trnsy(unit.rotation - 90, 4, 5),
+			Time.time * 18
+		);
+		Draw.rect(
+			"heavy-armaments-t5A_copter-rotor-2-1",
+			unit.x + Angles.trnsx(unit.rotation - 90, -4, 5),
+			unit.y + Angles.trnsy(unit.rotation - 90, -4, 5),
+			Time.time * -18
+		);
+		Draw.rect(
+			"heavy-armaments-t5A_copter-rotor-2-2",
+			unit.x + Angles.trnsx(unit.rotation - 90, -4, 5),
+			unit.y + Angles.trnsy(unit.rotation - 90, -4, 5),
+			Time.time * -18
+		);
+	}
+});
+//Thanks to evl#8935 for helping me with these!!
+//Need to figure out how to make this modular
 
-            //create 10 bullets at this tile's location with random rotation and velocity/lifetime
-            for(var i = 0; i < 15; i++){
-                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
-            }
-            //triggering consumption makes it use up the items it requires
-            tile.entity.cons.trigger()
-        }
-    }
-})
-
+//constructor
+t3A_copter.constructor = () => extend(UnitEntity, {});
+t4A_copter.constructor = () => extend(UnitEntity, {});
+t5A_copter.constructor = () => extend(UnitEntity, {});
